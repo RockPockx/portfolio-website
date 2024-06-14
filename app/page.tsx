9e4ce1df29658f9985b59795cd25c252c2f781e7
DiegoNathanael <diego.nathanael06@gmail.com>
2024-06-14 10:47:30 +0700

    added gtag

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
+      <GoogleAnalytics gaId="G-XYZ" />
     </main>
   );
 }
